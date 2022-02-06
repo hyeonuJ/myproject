592b7cf38dbe0dd1efb0f706107cd1e83d8d2296
hw020 <hw020123@naver.com>
2022-02-06 15:50:25 +0900

    3. 파이보 서비스 개발! study

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,22 +1,54 @@
 from datetime import datetime
-from flask import Blueprint, url_for, request, render_template
+from flask import Blueprint, url_for, request, render_template, g, flash
 from werkzeug.utils import redirect
 from pybo import db
 from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
+from .auth_views import login_required
 
 # answer_views.py 파일이 answer 라는 이름의 블루프린트 파일임을 나타냄
 bp = Blueprint('answer', '__name__', url_prefix='/answer')
 
 # methods 속성은 답변 저장 템플릿에 있는 form 엘리먼트의 method값과 일치해야 함
 @bp.route('/create/<int:question_id>', methods=('POST',))
+@login_required
 def create(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     if form.validate_on_submit():
         content = request.form['content']
-        answer = Answer(content=content, create_date=datetime.now())
+        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail',question_id=question_id))
+        return redirect('{}#answer_{}'.format(url_for('question.detail',question_id=question_id),answer.id))
     return render_template('question/question_detail.html',question=question, form=form)
+
+@bp.route('/modify/<int:answer_id>',methods=('GET', 'POST'))
+@login_required
+def modify(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash('수정권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=answer.question.id))
+    if request.method == "POST":
+        form = AnswerForm()
+        if form.validate_on_submit():
+            form.populate_obj(answer)
+            answer.modify_date = datetime.now() # 수정일시 저장
+            db.session.commit()
+            return redirect('{}#answer_{}'.format(url_for('question.detail',question_id=answer.question.id),answer.id))
+    else:
+        form = AnswerForm(obj=answer)
+    return render_template('answer/answer_form.html', answer=answer, form=form)
+
+@bp.route('/delete/<int:answer_id>')
+@login_required
+def delete(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    question_id = answer.question.id
+    if g.user != answer.user:
+        flash('삭제권한이 없습니다')
+    else:
+        db.session.delete(answer)
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=question_id))
